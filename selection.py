0f131b4315d0101d77cfccfd9e887467d20a1ebc
Kerwin <zhouzhitong2012@163.com>
2016-08-24 16:19:25 +0800

    finally_version

--- selection.py
+++ selection.py
@@ -2,6 +2,7 @@
 import csv
 import itertools
 import sys
+
 reload(sys)
 sys.setdefaultencoding('utf-8') 
 csvfile = file('2.csv', 'rb')
@@ -43,7 +44,7 @@ for i in itertools.combinations(("12003315","22003360","22003428","22003429","21
 				tmp.append(y)
 				tmp.append(z)
 				queuelist.append(tmp)
-sys.stdout = open('5star.txt','w')
+# sys.stdout = open('5star.txt','w')
 fivestar =[]
 for couses in queuelist:
 	counter = 0
@@ -52,52 +53,54 @@ for couses in queuelist:
 	if counter == 5:
 		# print couses
 		fivestar.append(couses)
-
+# fivestar 是全部已经优先选择的课程 
 allcourse=["12003315","22003360","22003428","22003429","21008300","21008305","11009010","21009306","11012002","21012001","24003378","24003496","14003309","22003468","22003334","12003346","22003494"]
+lists = []
 for ruler in range(9,13):
-	sys.stdout = open('%s.txt'%ruler,'w')
+	
+	star = []
+	nostar = []
 	for musthave in fivestar:
 		grade = 0
 		for course in musthave:
 			grade += int(dictgrade[course])
-		if grade == ruler:print "%s" %musthave
+		if grade == ruler:
+			star.append(musthave)
 
-	sys.stdout = open('%smatch.txt'%ruler,'w')
-	for i in itertools.combinations(allcourse, 2):
+	notin = [i for i in allcourse if i not in musthave] # 差集
+	for i in itertools.combinations(notin, 2):
 		list2 = list(i)
 		if 15-ruler == int(dictgrade[list2[0]])+int(dictgrade[list2[1]]):
-			print list2
-	# ,'grade = ',int(dictgrade[list2[0]])+int(dictgrade[list2[1]])
+			nostar.append(list2)
+	lists.append([star,nostar])
+
+
+# 此时lists 格式为 lists = [[9分列表,6分列表],[10分列表,5分列表],[11分列表,4分列表],[12分列表,3分列表]]
+
+comblist = []
+for i in range(4):
+    if lists[i][0] and lists[i][1]:
+        for x in lists[i][0]:
+            for y in lists[i][1]:
+                comblist.append(x+y)
+
+#comblist是所有分数为15分的合集
 
 
 
-numf = "11"
-filename = numf+".txt"
-file2name = numf+"match.txt"
-resultname = numf+"result"
-f1 = open(filename, 'r')
-f2 = open(file2name, 'r')
-sys.stdout = open(resultname,'w')
-
-for line in f1:
-	for line2 in f2:
-		print line+line2
-
-f = open('results.txt','r')
-resultlist = []
-for line in f:
-	line = line.split()
-	if len(set(line)) == 5:
-		print line
-		resultlist.append(line)
-
-for coursecomb in resultlist:
-	for course in coursecomb:
-		print dictname[course],
-	print
-
-for couses in resultlist:
+# for coursecomb in resultlist:
+# 	for course in coursecomb:
+# 		print dictname[course],
+# 	print
+sevenstar = []
+for courses in comblist:
 	counter = 0
-	for couse in couses:
-		if dictmark[couse] == 'O':counter+=1
-	print counter
+	for course in courses:
+		if dictmark[course] == 'O':
+			counter+=1
+	if counter == 7:
+		sevenstar.append(courses)
+
+# sevenstar 七星的满足要求的列表
+for i in sevenstar[0]:
+	print dictname[i]
